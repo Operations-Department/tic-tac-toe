7a7d301f41316191b93bdd29e75cff9cfd3e4ce9
Chris <129601042+Operations-Department@users.noreply.github.com>
2023-10-30 20:02:13 +0900

    Add mark iife, add x/o to grid array and dom grid

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 const gameBox = document.getElementById('game-box');
+const restart = document.getElementById('restart-button');
 
 //stores the game grid items
 const gameBoard = {
     grid: [
-        'x', 'o', 'o',
-        'o', 'x', 'x',
-        'o', 'x', 'x',
+        '', '', '',
+        '', '', '',
+        '', '', '',
     ]
 };
 
-//displays the grid items to the dom
-const displayController = {
+//displays the grid to the dom
+const gameController = {
     displayGrid: function() {
         return gameBoard.grid.forEach((cellContent, index) => {
-            const cell = document.createElement('div');
+            const cell = document.createElement('button');
             cell.textContent = cellContent;
 
             gameBox.appendChild(cell);
-            // cell.classList.add('game-square');
+            cell.classList.add('game-square');
+            cell.classList.add(`cell-${index}`);
+            cell.setAttribute('data-index', `${index}`);
         });
     }
-}
+};
 
-//puts game on screen when loaded
-document.addEventListener('DOMContentLoaded', displayController.displayGrid());
+//puts x/o mark into cell/array;
+const mark = (function() {
+    gameBox.addEventListener('click', function(event) {
+        if (event.target.textContent !== '') return;
+        let index = event.target.getAttribute('data-index');
+        event.target.textContent = 'X';
+        gameBoard.grid[index] = 'X';  
+        console.log(gameBoard.grid);
+    });
+})();
+
+//setup game/grid on screen load
+const setupGame = (function() {
+    document.addEventListener('DOMContentLoaded', () => {
+        gameController.displayGrid();
+    });
+})();
 
 //factory to create players
 const createPlayer = (function(name, symbol, order) {
@@ -35,8 +53,7 @@ const createPlayer = (function(name, symbol, order) {
             return `${this.name} has the symbol '${this.symbol}' and is ${this.order} in play order.`;
         }
     };
-});
+})();
 
 const player1 = createPlayer('Player1', 'X', 'first');
 const player2 = createPlayer('Player2', 'O', 'second');
-
