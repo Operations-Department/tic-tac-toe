a952ef5a868ce9fa65cd8a43a42f2e5502ad0a34
Chris <129601042+Operations-Department@users.noreply.github.com>
2023-11-01 17:56:11 +0900

    complete updateDisplay function and restartGame function

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const domVariables = {
+
     gameBox: document.getElementById('game-box'),
     restart: document.getElementById('restart-button'),
+    cell: document.querySelectorAll('.game-square'),
+    playerTurn: true,       //variable to toggle between players
+
 };
 
 //stores the game grid items
 const gameBoard = {
+
     grid: [
         '', '', '',
         '', '', '',
         '', '', '',
-    ]
+    ],
+
 };
 
 //sets up and displays the grid to the dom
 const gameController = {
+
+    //displays empty grid on page load
     displayGrid: (function() {
         return gameBoard.grid.forEach((cellContent, index) => {
             const cell = document.createElement('button');
@@ -22,49 +30,57 @@ const gameController = {
             domVariables.gameBox.appendChild(cell);
             cell.classList.add('game-square');
             cell.classList.add(`cell-${index}`);
-            cell.setAttribute('data-index', `${index}`);
+            cell.setAttribute('data-index', `${index}`); //to connect with array index
+
+            domVariables.cell = document.querySelectorAll('.game-square'); //ensures variables are assigned correctly and prevents error
         });
     })(),
-    markCell: (function() {
-        domVariables.gameBox.addEventListener('click', function(event) {
-            if (event.target.textContent !== '') return;
-            let index = event.target.getAttribute('data-index');
-            //give data-index to updateDisplay()
-        });
-    })(),
-    updateDisplay: (function() {
-        //update the display
-    })(),
-    restartGame: (function() {
-        domVariables.restart.addEventListener('click', () => {
-            gameBoard.grid = ['', '', '', '', '', '', '', '', ''];
-            //gameController.updateDisplay();
-        });
-    })(),
+
+    //updates the display to reflect gameBoard grid array
+    updateDisplay: function() {
+        for (let i = 0; i < gameBoard.grid.length; i++) {
+            domVariables.cell[i].textContent = gameBoard.grid[i];
+        }
+        console.log(gameBoard.grid);
+        return;
+    },
+
+    //send player marker to array, calls updateDisplay to show in grid
+    playRound: function(event) {
+        if (event.target.textContent !== '') return; // prevents from clicking the same square more than once
+
+        let index = event.target.getAttribute('data-index');
+
+        // gameBoard.grid[index] = 'X';
+        gameBoard.grid[index] = 'O';
+
+        gameController.updateDisplay();
+    },
+
+    //empties array and calls updateDisplay to reflect empty array
+    restartGame: function() {
+        gameBoard.grid = ['', '', '', '', '', '', '', '', ''];
+        gameController.updateDisplay();
+    },
+
 };
 
-// //puts x/o mark into cell & array;
-// const markCell = (function() {
-//     domVariables.gameBox.addEventListener('click', function(event) {
-//         if (event.target.textContent !== '') return;
-//         let index = event.target.getAttribute('data-index');
-//         event.target.textContent = 'X';
-//         gameBoard.grid[index] = 'X';  
-//         console.log(gameBoard.grid);
-//     });
-// })();
+domVariables.gameBox.addEventListener('click', gameController.playRound);
+domVariables.restart.addEventListener('click', gameController.restartGame);
 
 //factory to create players
-const createPlayer = function (name, symbol, order) {
-    return {
-        name,
-        symbol,
-        order,
-        getInfo() {
-            return `${this.name} has the symbol '${this.symbol}' and is ${this.order} in play order.`;
+const createPlayer = function (symbol) {
+
+    const player = {
+        symbol: symbol,
+        makeMark() {
+            return this.symbol;
         }
     };
+
+    return player;
+
 };
 
-const player1 = createPlayer('Player1', 'X', 'first');
-const player2 = createPlayer('Player2', 'O', 'second');
+const playerX = createPlayer('X');
+const playerO = createPlayer('O');
